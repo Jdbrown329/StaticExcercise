a0c41b930b35b1850589df20fd0f427688e215f6
Jordan Brown <j.d.brown329@gmail.com>
2022-09-26 13:59:02 -0500

    first commit

---
+++ StaticExcercise/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using StaticExcercise;
+
+var Celsius = TempConverter.FahrenheitToCelsius(0);
+var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+
+Console.WriteLine($"After conversion: Celsius {Celsius}");
+Console.WriteLine($"After conversion: Fahrenheit {fahrenheit}");
