24602187991276d3e3de43055c8776e9e09c6605
Jordan Brown <j.d.brown329@gmail.com>
2022-10-25 11:16:04 -0500

    finished

--- StaticExcercise/Program.cs
+++ StaticExcercise/Program.cs
@@ -1,9 +1,12 @@
 ﻿
 
+
+
 using StaticExcercise;
 
 var Celsius = TempConverter.FahrenheitToCelsius(0);
-var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
+    var fahrenheit = TempConverter.CelsiusToFahrenheit(0);
 
 Console.WriteLine($"After conversion: Celsius {Celsius}");
 Console.WriteLine($"After conversion: Fahrenheit {fahrenheit}");
+    
